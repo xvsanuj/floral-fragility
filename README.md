bed2870b6790f99e6cdf24ef9cf2ef3ff06c0eb9
Anuj Sharma <anuj.shar2k8@gmail.com>
2024-12-30 10:24:07 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# The Floral Fragility
+
+## How to run
+
+1. Clone the repository
+2. Run `npm install`
+3. Run `npm run dev`
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='fixed bottom-0 z-50 w-full flex items-center justify-between py-12 px-20'>
+        <h5 className='font-[ogg] text-[1vw] font-light'>Aug 28, 2024</h5>
+    </div>
+  )
+}
+
+export default Footer
